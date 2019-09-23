b1028eff7e4433d989a4eec9cfa0f6d591df928f
Jason Rees <iwhisperit@gmail.com>
2019-09-23 17:03:27 +0100

    Changes from review

--- index.php
+++ index.php
@@ -7,15 +7,13 @@
 <body>
 <?php
 //This connects to my database
-$db=new PDO('mysql:host=db;dbname=netflix_series','root','password');
+$db = new PDO('mysql:host=db;dbname=netflix_series','root','password');
 //Fixes the format(one instead of two values)
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
 // Run $db's query method with the SQL passed to it - from is the table name
 $query = $db-> query('SELECT `id`,`TITLE`,`START DATE`,`END DATE`,`NUMBER OF SEASONS`,`NUMBER OF EPISODES`,`DIRECTOR` FROM `favourites`');
-//execute the above
-$query->execute();
 // Run query's fetchAll method to translate info into something understandable
-$series=$query->fetchAll();
+$series = $query->fetchAll();
 // Outputs to browser with some HTML
 foreach($series as $serie) {
     echo '<div>';
