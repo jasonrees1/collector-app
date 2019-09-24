34454270139eba4fe0b32354c53e5446f4acd2f5
Jason Rees <iwhisperit@gmail.com>
2019-09-24 13:41:48 +0100

    requested changes before merge to master

--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>title</title>
+    <title>Netflix Series Collection</title>
 </head>
 <body>
 <?php
-//This connects to my database
 $db = new PDO('mysql:host=db;dbname=netflix_series','root','password');
-//Fixes the format(one instead of two values)
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-// Run $db's query method with the SQL passed to it - from is the table name
-$query = $db-> query('SELECT `id`,`TITLE`,`START DATE`,`END DATE`,`NUMBER OF SEASONS`,`NUMBER OF EPISODES`,`DIRECTOR` FROM `favourites`');
-// Run query's fetchAll method to translate info into something understandable
+$query = $db-> query('SELECT `id`,`title`,`start_date`,`end_date`,`number_of_seasons`,`number_of_episodes`,`director` FROM `favourites`');
 $series = $query->fetchAll();
-// Outputs to browser with some HTML
 foreach($series as $serie) {
     echo '<div>';
-    echo '<h2>Title: ' . $serie['TITLE'] . '</h2>';
-    echo '<p>Start Date: ' . $serie['START DATE'] . '</p>';
-    echo '<p>End Date: ' . $serie['END DATE'] . '</p>';
-    echo '<p>Number Of Seasons: ' . $serie['NUMBER OF SEASONS'] . '</p>';
-    echo '<p>Number Of Episodes: ' . $serie['NUMBER OF EPISODES'] . '</p>';
-    echo '<p>Director: ' . $serie['DIRECTOR'] . '</p>';
+    echo '<h2>Title: ' . $serie['title'] . '</h2>';
+    echo '<p>Start Date: ' . $serie['start_date'] . '</p>';
+    echo '<p>End Date: ' . $serie['end_date'] . '</p>';
+    echo '<p>Number Of Seasons: ' . $serie['number_of_seasons'] . '</p>';
+    echo '<p>Number Of Episodes: ' . $serie['number_of_episodes'] . '</p>';
+    echo '<p>Director: ' . $serie['director'] . '</p>';
     echo '</div>';
 }
 ?>
