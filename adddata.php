1baec08cb8f6caeeca1f315dc9ab7acb90e23a8c
Jason Rees <iwhisperit@gmail.com>
2019-09-24 16:56:40 +0100

    redirect to work on 1 page

--- adddata.php
+++ adddata.php
@@ -1,10 +1,5 @@
 <?php
-echo $_POST ["seriestitle"];
-echo $_POST ["datestarted"];
-echo $_POST ["dateended"];
-echo $_POST ["noofseasons"];
-echo $_POST ["noofepisodes"];
-echo $_POST ["director"];
 $db = new PDO('mysql:host=db;dbname=netflix_series','root','password');
 $query = $db->prepare('INSERT INTO `favourites`(`title`,`start_date`,`end_date`,`number_of_seasons`,`number_of_episodes`,`director`) VALUES (:title, :start_date, :end_date, :number_of_seasons, :number_of_episodes, :director)');
 $query -> execute(['title' => $_POST ["seriestitle"], 'start_date' => $_POST ["datestarted"], 'end_date' => $_POST ["dateended"], 'number_of_seasons' => $_POST ["noofseasons"], 'number_of_episodes' => $_POST ["noofepisodes"], 'director' => $_POST ["director"]]);
+header('Location: index.php');
--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 <?php
 $db = new PDO('mysql:host=db;dbname=netflix_series','root','password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-$query = $db-> query('SELECT `id`,`title`,`start_date`,`end_date`,`number_of_seasons`,`number_of_episodes`,`director` FROM `favourites`');
+$query = $db->query('SELECT `id`,`title`,`start_date`,`end_date`,`number_of_seasons`,`number_of_episodes`,`director` FROM `favourites`');
+$query->execute();
 $series = $query->fetchAll();
 foreach($series as $serie) {
     echo '<div>';
@@ -21,5 +22,22 @@ foreach($series as $serie) {
     echo '</div>';
 }
 ?>
+<h2>Complete all fields to add a new series to your collection</h2>
+<form method="post" action="adddata.php">
+    Series Title:<br>
+    <input type="text" name="seriestitle"><br>
+    Date Started:<br>
+    <input type="date" name="datestarted"><br>
+    Date Ended:<br>
+    <input type="date" name="dateended"><br>
+    Number of seasons:<br>
+    <input type="number" name="noofseasons" min="1" max="50"><br>
+    Number of episodes:<br>
+    <input type="number" name="noofepisodes" min="1" max="300"><br>
+    Director:<br>
+    <input type="text" name="director"><br>
+    <input type="submit" value="submit">
+
+</form>
 </body>
 </html>
